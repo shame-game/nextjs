fd9435f9e7c8f91c40d9c2ee1716cf00231d9aeb
shame-game <hoanghung.vam1209@gmail.com>
2023-10-14 02:20:48 +0700

    d

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,23 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import { Header }  from '../conponents/Header'
+import { Footer }  from '../conponents/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Nextjs',
+  title: 'Vamnaone',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
---
+++ src/conponents/Footer/index.js
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css'
+
+export const Footer = () => {
+  return (
+    <footer>
+        <div>Footer</div>
+    </footer>
+  )
+}
+
